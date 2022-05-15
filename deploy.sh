01a27636e7d28a71b52ec5c7f2e29075ccf78a24
ian <ian@iandeMacBook-Air.local>
2022-05-15 14:09:06 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:leeyencheng129/HBD_TEST.git master:gh-pages
+git push -f git@github.com:leeyencheng129/tt.git master:gh-pages
 
 cd -
